e07801a4ef9c117eff942f179fe9ac1687423bcd
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2021-04-15 03:36:46 +0530

    Add files via upload

--- task2part1.js
+++ task2part1.js
@@ -4,19 +4,22 @@ request.send();
 var population;
 request.onload = function() {
 var data = JSON.parse(this.response);
-    for(var i in data){
-       
-        console.log(data[i].population);
+    var totalpopulation = 0;
+    for(var i=0;i<data.length;i++){
+        totalpopulation = totalpopulation + data[i].population;
     }
+    console.log(totalpopulation);
+     
+    
+    
     
             
-    
-        var request2 = new XMLHttpRequest();
-        var url2 = "http://api.openweathermap.org/data/2.5/weather?q="+population+"&appid=bb29024b3306194e6f8cd4f9965abffe"
-        request2.open('GET',url2,true);
-        request2.send(population);
-        request2.onload = function() {
-        var data2 = JSON.parse(this.response);
-        console.log(data2);    
+           var request2 = new XMLHttpRequest();
+            
+            request2.open('GET',"http://api.openweathermap.org/data/2.5/weather?q="+population+"&appid=bb29024b3306194e6f8cd4f9965abffe",true);
+            request2.send(population);
+            request2.onload = function() {
+            var data2 = JSON.parse(this.response);
+            console.log(data2);    
+        }
     }
-}
